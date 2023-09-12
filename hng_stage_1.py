5c393ebca862c2c93fb16acd331ed0fd7b9beec9
Ayodeji Oni <88333351+Ayo-Oni-515@users.noreply.github.com>
2023-09-12 13:50:22 +0100

    Week 1 project => Endpoint

---
+++ hng_stage_1.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import datetime
+
+my_application = Flask(__name__)
+
+url = {'backend' : 'https://github.com/Ayo-Oni-515/HNG_BACKEND.git'}
+
+@my_application.route("/my_endpoint", methods=['GET'])
+def get_info():
+    first_argument = request.args.get('slack_name')
+    second_argument = request.args.get('track')
+    day = datetime.datetime.utcnow().strftime('%A')
+    time = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
+    
+
+    information = {
+        "slack_name": first_argument,
+        "current_day": day,
+        "utc_time": time,
+        "track": second_argument,
+        "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
+        "github_repo_url": "https://github.com/Ayo-Oni-515/HNG_BACKEND.git",
+        "status_code": 200
+    }
+
+
+
+if __name__ == '__main__':
+    my_application.run(debug=True)
